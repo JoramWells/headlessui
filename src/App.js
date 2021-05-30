42bba064cf9676242e73c19f40a89f133eae951c
unknown <jorammanoah1@gmail.com>
2021-05-30 13:10:27 -0700

    Dialogmodal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import DialogModal from './components/DialogModel';
 import HeadTag from './components/HeadTag'
 
 function App() {
   return (
     <>
     <HeadTag/>
+    <DialogModal/>
 
     </>
   );
